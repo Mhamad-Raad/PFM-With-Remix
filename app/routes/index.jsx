ba8fe7b76d6b1691e86086f37719eb5c6481ec36
joker69216 <hamaraad883@gmail.com>
2023-01-24 15:53:36 +0300

    getting data from firebase

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { redirect } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
 import UpBar, { links as UpBarStyles } from "~/components/UpBar"
 import { links as ModalStyles } from "~/components/Modal/AddTransactionModal"
 import Modal from "~/components/Modal/AddTransactionModal"
@@ -9,6 +10,14 @@ import AddTransactionButton, {
 
 export default function Overview() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const transactions = useLoaderData()
+
+  // using later
+  // Object.keys(transactions).forEach(function (key, index) {
+  //   console.log('key', key)
+  //   console.log('transactions[key]', transactions[key])
+  // })
 
   const switchModaltoTrue = () => {
     setIsModalOpen(true)
@@ -27,14 +36,24 @@ export default function Overview() {
   )
 }
 
+export async function loader() {
+  const res = await fetch(
+    "https://cash-management-c186c-default-rtdb.firebaseio.com/changes.json"
+    )
+  return res
+}
+
 export async function action({ request }) {
   const formData = await request.formData()
   const body = Object.fromEntries(formData.entries())
-  
-  await fetch("https://cash-management-c186c-default-rtdb.firebaseio.com/changes.json", {
-    method: "POST",
-    body: JSON.stringify(body),
-  })
+
+  await fetch(
+    "https://cash-management-c186c-default-rtdb.firebaseio.com/changes.json",
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+    }
+  )
 
   return redirect("/")
 }
