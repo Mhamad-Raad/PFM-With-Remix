193d04ceecf43c1bbed9aef505216b4f042880ff
joker69216 <hamaraad883@gmail.com>
2023-01-25 13:32:58 +0300

    add the date sorting algorithm to show transactions accordingly

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -11,6 +11,26 @@ import ListMoneyTags, {
   links as ListMoneyTagsStyle,
 } from "~/components/MoneyTags/ListMoneyTags"
 
+const getWeekNumber = (date) => {
+  const currentDate = new Date()
+  const startDate = date
+  var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000))
+
+  var weekNumber = Math.ceil(days / 7)
+
+  return weekNumber
+}
+
+const getCurrentWeekNumber = () => {
+  const currentDate = new Date()
+  const startDate = new Date(currentDate.getFullYear(), 0, 1)
+  var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000))
+
+  var weekNumber = Math.ceil(days / 7)
+
+  return weekNumber
+}
+
 export default function Overview() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
@@ -19,16 +39,60 @@ export default function Overview() {
   let userBalance = 0
   let userIncome = 0
   let userExpense = 0
+  let date
 
-  Object.keys(transactions).forEach(function (key, index) {
+  const currentDateYear = new Date().getFullYear()
+  const currentDateMonth = new Date().getMonth()
+  const currentWeek = getCurrentWeekNumber()
+  let thisYearTransactions = []
+  let thisMonthTransactions = []
+  let thisWeekTransactions = []
+  let showntransactions = []
+
+  Object.keys(transactions)?.forEach(function (key) {
     if (transactions[key].type === "income") {
       userIncome += +transactions[key].amount
     } else {
       userExpense += +transactions[key].amount
     }
+    const date = new Date(transactions[key].date)
+    if (date.getFullYear() === currentDateYear) {
+      thisYearTransactions.push({
+        ...transactions[key],
+        id: key,
+      })
+      if (date.getMonth() === currentDateMonth) {
+        thisMonthTransactions.push({
+          ...transactions[key],
+          id: key,
+        })
+        if (getWeekNumber(date) === currentWeek) {
+          thisWeekTransactions.push({
+            ...transactions[key],
+            id: key,
+          })
+        }
+      }
+    }
   })
-
   userBalance = userIncome - userExpense
+
+  if (thisWeekTransactions.length > 0) {
+    if (thisWeekTransactions.length > 10) {
+      thisWeekTransactions = thisWeekTransactions.slice(0, 10)
+    }
+    showntransactions = thisWeekTransactions
+  } else if (thisMonthTransactions.length > 0) {
+    if (thisMonthTransactions.length > 10) {
+      thisMonthTransactions = thisMonthTransactions.slice(0, 10)
+    }
+    showntransactions = thisMonthTransactions
+  } else if (thisYearTransactions.length > 0) {
+    if (thisYearTransactions.length > 10) {
+      thisYearTransactions = thisYearTransactions.slice(0, 10)
+    }
+    showntransactions = thisYearTransactions
+  }
 
   const switchModaltoTrue = () => {
     setIsModalOpen(true)
@@ -58,6 +122,7 @@ export async function loader() {
   const res = await fetch(
     "https://cash-management-c186c-default-rtdb.firebaseio.com/changes.json"
   )
+
   return res
 }
 
