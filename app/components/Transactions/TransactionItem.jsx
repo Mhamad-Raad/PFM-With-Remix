530e7b01e73fad4c9db048f4387cafe9777c873d
joker69216 <hamaraad883@gmail.com>
2023-01-25 17:28:10 +0300

    add style to the transaction item description

--- app/components/Transactions/TransactionItem.jsx
+++ app/components/Transactions/TransactionItem.jsx
@@ -1,9 +1,26 @@
 import TransactionItemStyle from "~/styles/components/Transactions/TransactionItem.css"
+import WorkIcon from "~/assets/Icons/WorkIcon"
+import StudyIcon from "~/assets/Icons/StudyIcon"
+import TechIcon from "~/assets/Icons/TechIcon"
+
 export default function TransactionItem({ transaction }) {
+  let ShownIcon
+  if (transaction.category === "work") {
+    ShownIcon = WorkIcon
+  } else if (transaction.category === "Tech") {
+    ShownIcon = TechIcon
+  } else {
+    ShownIcon = StudyIcon
+  }
   return (
     <div className="item row">
       <div className="item__left row">
-        
+          <ShownIcon />
+        <div className="item__left__text">
+          <p className="item__left__text__title">
+            {transaction.description}
+          </p>
+        </div>
       </div>
     </div>
   )
