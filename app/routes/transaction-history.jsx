6c7dd24f2e3d9f290af1f6d97ea19a2790fc2f32
joker69216 <hamaraad883@gmail.com>
2023-01-26 15:23:13 +0300

    add needed Icons for the history page

--- app/routes/transaction-history.jsx
+++ app/routes/transaction-history.jsx
@@ -4,6 +4,11 @@ export default function transaction_history() {
   return (
     <div className="transaction-history-page column">
       <UpBar title={"Transaction History"} />
+      <span className='search-bar'>
+        
+        <input type="text" placeholder="Search" />
+
+      </span>
     </div>
   )
 }
