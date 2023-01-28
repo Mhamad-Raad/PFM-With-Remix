ba417e0ad06729645b214e17c212c68add0f4037
joker69216 <hamaraad883@gmail.com>
2023-01-28 19:09:05 +0300

    transactions to the transaction history

--- app/components/FiltersBar.jsx
+++ app/components/FiltersBar.jsx
@@ -1,10 +1,19 @@
-import FilterBarIcon from '~/assets/Icons/FilterBarIcon'
+import FilterBarIcon from "~/assets/Icons/FilterBarIcon"
+import FiltersBarStyle from "~/styles/components/FilterBar.css"
 
 export default function FiltersBar() {
   return (
     <div className="filters">
       <FilterBarIcon />
-      
     </div>
   )
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: FiltersBarStyle,
+    },
+  ]
 }
--- app/routes/transaction-history.jsx
+++ app/routes/transaction-history.jsx
@@ -1,13 +1,43 @@
+import { useLoaderData } from "@remix-run/react"
+
 import UpBar from "~/components/UpBar"
 import SearchBar from "~/components/SearchBar"
+import FiltersBar, { links as FiltersBarStyle } from "~/components/FiltersBar"
+import TransactionList, {
+  links as TransactionListStyle,
+} from "~/components/Transactions/TransactionList"
 
 export default function transaction_history() {
+  const transactions = useLoaderData()
+  let allTransactions = []
+
+  Object.keys(transactions)?.forEach((key) => {
+    allTransactions.push({
+      ...transactions[key],
+      id: key,
+    })
+  })
+
   return (
     <div className="transaction-history-page column">
       <UpBar title={"Transaction History"} />
       <main>
         <SearchBar />
+        <FiltersBar />
+        <TransactionList transactions={allTransactions} />
       </main>
     </div>
   )
+}
+
+export async function loader() {
+  const res = await fetch(
+    "https://cash-management-c186c-default-rtdb.firebaseio.com/changes.json"
+  )
+
+  return res
+}
+
+export function links() {
+  return [...TransactionListStyle(), ...FiltersBarStyle()]
 }
