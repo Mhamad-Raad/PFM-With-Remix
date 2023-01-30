bed5a838a1a9f793bbe9f3f2ce11ad7d3af66bd0
joker69216 <hamaraad883@gmail.com>
2023-01-30 12:12:54 +0300

    test routes again

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -11,9 +11,9 @@ import AddTransactionButton, {
 import ListMoneyTags, {
   links as ListMoneyTagsStyle,
 } from "~/components/MoneyTags/ListMoneyTags"
-import TransactionList, {
-  links as TransactionListStyle,
-} from "~/components/Transactions/TransactionList"
+// import TransactionList, {
+//   links as TransactionListStyle,
+// } from "~/components/Transactions/TransactionList"
 
 const getWeekNumber = (date) => {
   const weekNumber = Math.floor(date.getDate() / 7)
@@ -120,7 +120,7 @@ export default function Overview() {
           <h1 className="overview-page__content__transaction-title">
             {shownTitle}
           </h1>
-          <TransactionList transactions={showntransactions} />
+          {/* <TransactionList transactions={showntransactions} /> */}
           <div className="overview-page__content__transaction-btn-bg">
             <AddTransactionButton clickHandler={switchModaltoTrue} />
           </div>
@@ -190,6 +190,6 @@ export function links() {
     ...ModalStyles(),
     ...AddTransactionButtonStyles(),
     ...ListMoneyTagsStyle(),
-    ...TransactionListStyle(),
+    // ...TransactionListStyle(),
   ]
 }
