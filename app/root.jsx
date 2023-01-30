4d9f40829a41a551066f4325a41934a4faffe5ee
joker69216 <hamaraad883@gmail.com>
2023-01-30 12:43:30 +0300

    test functions

--- app/root.jsx
+++ app/root.jsx
@@ -8,8 +8,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react"
 
-import rootStyles from "~/styles/root.css"
-import transaction_historyStyles from "~/styles/pages/transaction-history.css"
+// import rootStyles from "~/styles/root.css"
+// import transaction_historyStyles from "~/styles/pages/transaction-history.css"
 // import MainNavigation, {
 //   links as MainNavigationStyles,
 // } from "~/components/MainNavigation"
@@ -19,27 +19,27 @@ import transaction_historyStyles from "~/styles/pages/transaction-history.css"
  * @returns {import("@remix-run/node").LinkDescriptor[]}
  */
 
-export const links = () => [
-  {
-    rel: "stylesheet",
-    href: rootStyles,
-  },
-  {
-    rel: "stylesheet",
-    href: transaction_historyStyles,
-  },
-  // ...MainNavigationStyles(),
-  // ...UpBarStyles(),
-]
+// export const links = () => [
+//   {
+//     rel: "stylesheet",
+//     href: rootStyles,
+//   },
+//   {
+//     rel: "stylesheet",
+//     href: transaction_historyStyles,
+//   },
+//   // ...MainNavigationStyles(),
+//   // ...UpBarStyles(),
+// ]
 
 /**
  * @returns {import("@remix-run/node").MetaFunction}
  */
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-})
+// export const meta = () => ({
+//   charset: "utf-8",
+//   title: "New Remix App",
+//   viewport: "width=device-width,initial-scale=1",
+// })
 
 export default function App() {
   return (
@@ -61,27 +61,27 @@ export default function App() {
   )
 }
 
-export function ErrorBoundary({ error }) {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-        <title>an error occured!</title>
-      </head>
-      <body>
-        <div className="error">
-          <h1>an error occured!</h1>
-          <p>error: {error.message}</p>
-          <p>
-            Back to <Link to="/">safety!</Link>
-          </p>
-          <p>or come back later</p>
-        </div>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  )
-}
+// export function ErrorBoundary({ error }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <Meta />
+//         <Links />
+//         <title>an error occured!</title>
+//       </head>
+//       <body>
+//         <div className="error">
+//           <h1>an error occured!</h1>
+//           <p>error: {error.message}</p>
+//           <p>
+//             Back to <Link to="/">safety!</Link>
+//           </p>
+//           <p>or come back later</p>
+//         </div>
+//         <ScrollRestoration />
+//         <Scripts />
+//         <LiveReload />
+//       </body>
+//     </html>
+//   )
+// }
